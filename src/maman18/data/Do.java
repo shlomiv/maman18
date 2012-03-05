7f30cd189dfedba1da49cf4f9dd5055ed7a740fe
Shlomi <shlomivaknin@gmail.com>
2012-03-05 16:48:05 +0200

    added documentation

--- src/maman18/data/Do.java
+++ src/maman18/data/Do.java
@@ -1,5 +1,11 @@
 package maman18.data;
 
+/**
+ * @author Shlomi.v
+ *
+ * @param <T> - the type of the parameter
+ * a generic callback interface, act as a little lambda expression
+ */
 public interface Do<T> {
 	public void action(T t);
 }
